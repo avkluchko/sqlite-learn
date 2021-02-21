dfc4a5f856d0f81cd8539d06d30bb097381760a0
Aleksandr Klyuchko <avkluchko@yandex.ru>
2021-02-21 12:56:10 +0500

    Added Open Database code

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,20 @@
 #include <iostream>
-#include <string>
+#include <sqlite3.h>
 
 using namespace std;
 
 int main() {
+    sqlite3 *db;
+//    char *zErrMsg = 0;
+    int rc;
+
+    rc = sqlite3_open("test.db", &db);
+
+    if (rc) {
+        fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
+    } else {
+        fprintf(stderr, "Opened database successfully\n");
+    }
 
     return 0;
 }
